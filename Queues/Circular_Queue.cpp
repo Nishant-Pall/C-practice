6a67d1bdb8bbd158b8703fa11d4cb4eb3acf94e7
nishant <palln6935@gmail.com>
2020-04-10 16:31:46 +0530

    added circular queue class

---
+++ Queues/Circular_Queue.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+class Queue
+{
+	private:
+		int size;
+		int front;
+		int rear;
+		T *Q;
+	public:
+		Queue()
+		{
+			size = 10;
+			front = rear = -1;
+			Q = new T[size];
+		}
+		Queue(int size)
+		{
+			front = rear = -1;
+			this->size = size;
+			Q = new T[this->size];
+		}
+		void enqueue(T x);
+		T dequeue();
+		void display();
+};
+template<class T>
+/*
+	The below function is for enqueue, as the rear approaches the last, we can move it to the front by the statement in
+	if, for rear less than the size, rear will keep moving forward but once it reaches the end, the condtion will become zero
+	and it will move back to 0.
+*/
+void Queue<T>::enqueue(T x)
+{
+	if((rear + 1)%size == front)		
+		cout<<"Queue Overflow";
+	else
+	{
+		rear = (rear + 1)%size;
+		Q[rear] = x;
+	}
+}
+template<class T>
+T Queue<T>::dequeue()
+{
+	T x = -1;
+	if(rear = front)
+		cout<<"Queue is Empty";
+	else
+	{
+		front = (front + 1)%size;
+		x = Q[front];
+	}
+	return x;
+}
+template<class T>
+void Queue<T>::display()
+{
+	for(int i = front+1 ; i<=rear;i++)
+		cout<<Q[i]<<endl;
+}
+
+int main()
+{ 
+	Queue<int> q(10);
+	
+	q.enqueue(10);
+	q.enqueue(20);
+	q.enqueue(30);
+	q.enqueue(40);
+	
+	
+	q.display();
+	return 0;
+}
