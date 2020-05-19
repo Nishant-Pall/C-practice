a9574829c073d5b062dfa5fc3cac1e857394e000
nishant <palln6935@gmail.com>
2020-05-19 12:41:07 +0530

    added count sort

---
+++ Sorting Techniques/CountSort.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+int FindMax(int A[],int n)
+{
+	int max = -32578;
+	for(int i = 0; i<n; i++)
+	{
+		if(A[i] > max)
+			max = A[i];
+	}
+	return max;
+}
+
+void CountSort(int A[], int n)
+{
+	int max;
+	int *C;
+	int i,j;
+	max = FindMax(A,n);
+	C = new int[max+1];
+	
+	for(i = 0; i<max+1;i++)
+		C[i]=0;
+	
+	for(i =0; i<n;i++)
+		C[A[i]]++;
+		
+	i = 0;
+	j = 0;	
+	
+	while(i<max+1)
+	{
+		if(C[i]>0)
+		{
+			A[j++] = i;
+			C[i]--;
+		}
+		else
+			i++;
+	}
+}
+
+int main()
+{
+	int A[] = {6,3,9,10,15,6,8,12,3,6};
+	CountSort(A,10);
+	for(int i =0;i<10;i++)
+	{
+		cout<<A[i]<<endl;
+	}
+	return 0;
+}
