33a1bb97ee3f603af810fbb623e8ef9b4c0f2508
nishant <palln6935@gmail.com>
2020-05-31 11:00:30 +0530

    added Prims min cost spanning tree

--- Graphs/BFS.cpp
+++ Graphs/BFS.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 queue<int> q;
-int visited[8]={0};
+int visited[8]={0};		// for keeping track of vertices which are visited
 
 void BFS(int A[][8],int i)
 {
 	int u;
 	cout<<i<<endl;
 	visited[i]=1;
-	q.push(i);
+	q.push(i);		// pushing the vertex into the queue
 	while(!q.empty())
 	{
-		u = q.back();
+		u = q.back();	//popping out the last vertex
 		q.pop();
-		for(int v =1;v<8;v++)
-		{
-			if(A[u][v]==1 && visited[v]==0)
-			{
+		for(int v =1;v<8;v++)	// now for the vertex popped out,
+		{						// this loop will traverse along the adjacent matrix A 
+			if(A[u][v]==1 && visited[v]==0)	// if the the vertices found are not visited before and they're
+			{								// connected to u then the if block will execute itself
 				cout<<v<<endl;
 				visited[v]=1;
-				q.push(v);
+				q.push(v);			// push the vertex into the queue
 			}
 		}
 	}
