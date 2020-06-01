888245da7435d0a2f83792914e9c25ed2ca904aa
nishant <palln6935@gmail.com>
2020-06-01 11:16:24 +0530

    documentation and comments

--- Graphs/Prims_Min_Cost_Spanning_Tree.cpp
+++ Graphs/Prims_Min_Cost_Spanning_Tree.cpp
@@ -18,6 +18,7 @@ int t[2][6];
 
 int main(){
 	int i,j,k,u,v,n=7,min=I;
+//	The following for loop for getting the minimum cost edge from the tree
 	for(i=1; i<=n; i++){
 		for(j=i;j<=n;j++){
 			if(cost[i][j] < min){
@@ -27,29 +28,35 @@ int main(){
 			}
 		}
 	}
-	t[0][0] = u;
-	t[1][0] = v;
+	t[0][0] = u; // adding the min edge in t
+	t[1][0] = v; // adding the min edge in t
 	near[u] = near[v] = 0;
+//	Initializing the near array
 	for(i=1; i<=n; i++){
 		if(near[i]!=0){
 			if(cost[i][u] < cost[i][v])
-				near[i] = u;
+				near[i] = u;		// if i is near to u, mark it as u
 			else
-				near[i] = v;
+				near[i] = v;		// if i is near to v, mark it as v
 		}		
 	}
 	for(i=1; i<n-1; i++){
 		min = I;
 		for(j=1; j<=n; j++){
+//			This for loop is for getting the min cost edge from near array
 			if(near[j]!=0 && cost[j][near[j]] < min){
 				min = cost[j][near[j]];
 				k=j;
 			}
 		}
-		t[0][i] = k;
-		t[1][i] = near[k];
-		near[k] = 0;
+		t[0][i] = k; 		// storing the edge in t
+		t[1][i] = near[k]; // storing the edge in t
+		near[k] = 0;	// marking it as zero means we've included the vertex in t
 		for(j=1;j<=n;j++){
+/*	This for loop is for updating the near array when we have included the next min edge,
+	Now we will compare near[j] with the latest vertex and and previous vertex so as to get
+	min edge in next iteration
+*/
 			if(near[j]!=0 && cost[j][k] < cost[j][near[j]])
 				near[j] = k;
 		}
