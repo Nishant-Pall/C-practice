2502cb7d04ea611a7847720262eee3ad93186db2
nishant <palln6935@gmail.com>
2020-09-05 22:07:48 +0530

    add auto var declaration

--- auto_variable_declaration.cpp
+++ auto_variable_declaration.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 int main()
 {
-auto x=5*2.34 + 'a';
-cout<<x;
-return 0;
+	auto x=5*2.34 + 'a';
+	cout<<x;
+	int A[] = {1,2,3,4,5};
+	cout<<endl;
+	for(auto i : A){
+		cout<<i<<endl;
+	}
+	return 0;
 }
